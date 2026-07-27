c75d78154a9e868c1c60e45b9576294a563e3e0e
Moisés Alencar <moisesalencar456@gmail.com>
2026-07-27 14:02:05 -0300

    feat: add OpenAI LLM request

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("OpenRouter API not found")
+
+
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+messages = [
+    {
+        "role": "user",
+        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    }
+]
+
+response = client.chat.completions.create(model= "openrouter/free", messages= messages)
+
+if response.usage == None:
+    raise RuntimeError("Failed API request")
+print(f"Prompt tokens: {response.usage.prompt_tokens}")
+print(f"Response tokens: {response.usage.completion_tokens}")
+print(response.choices[0].message.content)
+
 def main():
-    print("Hello from mega-brain!")
-
-
-if __name__ == "__main__":
-    main()
+    # print("Hello from mega-brain!")
+    if __name__ == "__main__":
+        main()
