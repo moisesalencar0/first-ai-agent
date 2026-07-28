d5cfa3254964bf156dfc40e6f6587d2ab1e0722e
Moisés Alencar <moisesalencar456@gmail.com>
2026-07-28 10:06:40 -0300

    feat: use argsparse for CLI prompts

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+import argparse
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
-
-if api_key == None:
-    raise RuntimeError("OpenRouter API not found")
-
-
 from openai import OpenAI
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
+def generate_content(client, messages, user_prompt, verbose= False):
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+    if response.usage == None:
+            raise RuntimeError("Failed API request")
 
-messages = [
-    {
-        "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    }
-]
-
-response = client.chat.completions.create(model= "openrouter/free", messages= messages)
-
-if response.usage == None:
-    raise RuntimeError("Failed API request")
-print(f"Prompt tokens: {response.usage.prompt_tokens}")
-print(f"Response tokens: {response.usage.completion_tokens}")
-print(response.choices[0].message.content)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+     
+    print(response.choices[0].message.content)
 
 def main():
-    # print("Hello from mega-brain!")
-    if __name__ == "__main__":
-        main()
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("OpenRouter API not found")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": args.user_prompt,
+        }
+    ]
+
+    generate_content(client, messages, args.user_prompt, args.verbose)    
+
+if __name__ == "__main__":
+    main()
