4b4e5de40ec8525eba8de215755feda86d9c0305
Moisés Alencar <moisesalencar456@gmail.com>
2026-07-31 09:49:38 -0300

    feat: add function to allow LLM running python subprocesses

---
+++ functions/run_python_file.py
@@ -0,0 +1,52 @@
+import os, subprocess
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+
+        # PATH VALIDATION
+        valid_target_file = (  # Will be True or False
+            os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        )
+
+        if not valid_target_file:
+            return (
+                f'Error: Cannot execute "{file_path}"'
+                ' as it is outside the permited working directory'
+            )
+        
+        if not os.path.isfile(target_file): 
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+
+        if not target_file.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        command = ["python", target_file]
+        if args is not None:
+            command.extend(args)
+        
+
+        result = subprocess.run(
+            command,
+            cwd=working_directory,
+            capture_output=True,
+            text=True,
+            timeout=30
+        )
+
+        parts = []
+        if result.returncode != 0:
+            parts.append(f"Process exited with code {result.returncode}")
+        if result.stderr == "" and result.stdout == "":
+            parts.append("No output produced")
+        else:
+            parts.append(f"STDERR:{result.stderr}")
+            parts.append(f"STDOUT:{result.stdout}")
+
+        return "\n".join(parts)
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
